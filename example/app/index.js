c18ce25aceb8c6ba31a75065820bb5083a7cdf1c
Eugeniy Boyarskiy <gekabrs@gmail.com>
2016-03-24 15:02:35 +0200

    update example

--- example/app/index.js
+++ example/app/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import DatePicker from 'react-simple-datepicker';
+import 'react-simple-datepicker/dist/index.css';
+import './index.css';
 
-ReactDOM.render(
-  <App/>,
-  document.querySelector('.app')
-);
+const App = () => {
+  return (
+    <div>
+      <h3>Default</h3>
+      <DatePicker />
+
+      <h3>Custom onClick</h3>
+      <DatePicker clickOnDate={date => alert(date)} />
+
+      <h3>Date Range</h3>
+      <DatePicker date={new Date('03.15.16')} minDate={new Date('03.10.16')} maxDate={new Date('03.20.16')} />
+
+      <h3>Custom classNames</h3>
+      <DatePicker monthClassName='month' prevMonthClassName='prev' nextMonthClassName='next' />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('.app'));
